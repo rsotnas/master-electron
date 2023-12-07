b818ee91d1147649c9742566e2de98c858e52ce0
Felipe <fas.felipesantos@gmail.com>
2023-12-07 12:10:50 -0300

    .

--- main.js
+++ main.js
@@ -1,45 +1,72 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require("electron");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow, secondWindow;
 
 // Create a new BrowserWindow when `app` is ready
-function createWindow () {
-
+function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 1000, height: 800,
+    width: 1000,
+    height: 800,
+    // frame: false,
+    title: "My App",
+    titleBarStyle: "hiddenInset",
     webPreferences: {
       // --- !! IMPORTANT !! ---
       // Disable 'contextIsolation' to allow 'nodeIntegration'
       // 'contextIsolation' defaults to "true" as from Electron v12
       contextIsolation: false,
-      nodeIntegration: true
-    }
-  })
+      nodeIntegration: true,
+    },
+    // show: false
+  });
+
+  secondWindow = new BrowserWindow({
+    width: 600,
+    height: 300,
+    webPreferences: {
+      // --- !! IMPORTANT !! ---
+      // Disable 'contextIsolation' to allow 'nodeIntegration'
+      // 'contextIsolation' defaults to "true" as from Electron v12
+      contextIsolation: false,
+      nodeIntegration: true,
+    },
+    parent: mainWindow,
+    show: false,
+  });
 
   // Load index.html into the new BrowserWindow
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile("index.html");
+  secondWindow.loadFile("second.html");
 
   // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
+
+  // Show window when page is ready
+  // mainWindow.on('ready-to-show', () => {
+  //   mainWindow.show()
+  // })
 
   // Listen for window being closed
-  mainWindow.on('closed',  () => {
-    mainWindow = null
-  })
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+  secondWindow.on("closed", () => {
+    secondWindow = null;
+  });
 }
 
 // Electron `app` is ready
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed - (Not macOS - Darwin)
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
 
 // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
-app.on('activate', () => {
-  if (mainWindow === null) createWindow()
-})
+app.on("activate", () => {
+  if (mainWindow === null) createWindow();
+});
